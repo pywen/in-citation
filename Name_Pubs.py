a36037fc59cf216705b7d6dc233264bd7aa7445c
pywen <wenqidlut@hotmail.com>
2014-08-24 10:07:40 +0800

    Update Name_Pubs.py

--- Name_Pubs.py
+++ Name_Pubs.py
@@ -9,8 +9,24 @@ for i in range(1,2468):
     all_names.append(line['SELF'][0])
     for i in line['CR']:
         all_names.append(i[0])
-    self_pubs.append(line['SELF'][2])
-    all_pubs.append(line['SELF'][2])
+    if line['SELF'][2] is not None:
+        self_pubs.append(line['SELF'][2])
+        all_pubs.append(line['SELF'][2])
     for i in line['CR']:
         all_pubs.append(i[2])
 
+s_self_names=sorted(list(set(self_names)))
+s_all_names=sorted(list(set(all_names)))
+s_self_pubs=sorted(list(set(self_pubs)))
+s_all_pubs=sorted(list(set(all_pubs)))
+
+def write_content(List,File):
+    f=open(File,'w')
+    for i in List:
+        f.write(i+'\n')
+    f.close()
+
+write_content(s_self_names,'s-self-names.txt')
+write_content(s_all_names,'s-all-names.txt')
+write_content(s_self_pubs,'s-self-pubs.txt')
+write_content(s_all_pubs,'s-all-pubs.txt')
